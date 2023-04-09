618bca10f10726fbc597e1013ab7440913caddee
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-04-09 16:59:19 -0600

    main section cambios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { IoReorderThreeOutline } from 'react-icons/io5'
 import { BsBell } from 'react-icons/bs'
 import { FiEdit } from 'react-icons/fi'
+import { CiSearch } from 'react-icons/ci'
+import { HiChevronDown } from 'react-icons/hi'
 
 const HomePage = () => {
   return (
@@ -27,9 +29,52 @@ const HomePage = () => {
           </div>
         </div>
       </header>
-      <div className='grid grid-cols-12'>
-
-      </div>
+      <section className='grid grid-cols-12 place-items-center w-full h-full'>
+        <main className='col-span-8 border-r border-gray-300 w-full h-full'>
+          <div className='flex flex-col space-y-4 w-full py-10 px-24'>
+            <div className='flex space-x-4 items-center w-full'>
+              <label htmlFor='search' className='relative w-full border-gray-800 border rounded-3xl'>
+                <div className='absolute left-2 h-full flex items-center'>
+                  <CiSearch />
+                </div>
+                <input
+                  type="text"
+                  name="search"
+                  id="search"
+                  className='w-full rounded-3xl outline-none placeholder:text-gray-300 placeholder:text-xs text-sm py-1 px-4 pl-7'
+                  placeholder='Search...'
+                />
+              </label>
+              <div className='flex items-center w-full space-x-4 justify-end'>
+                <div>
+                  My topics:
+                </div>
+                <div className='flex space-x-2 items-center'>
+                  {
+                    Array.from({ length: 4 }).map((_, i) => (
+                      <div key={i} className='rounded-3xl bg-gray-200/50 px-4 py-3'>tag {i}</div>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
+            <div className='pb-8 w-full justify-between flex items-center border-b border-gray-300'>
+              <div>Articles</div>
+              <div>
+                <button className='flex items-center font-semibold space-x-2 rounded-3xl px-4 py-1.5 border-gray-800 border'>
+                  <div>Following</div>
+                  <div>
+                    <HiChevronDown className='text-xl' />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </main>
+        <aside className='col-span-4 w-full h-full'>
+          aside
+        </aside>
+      </section>
     </div>
   )
 }
