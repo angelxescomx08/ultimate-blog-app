0263463f3a403693798f7f09b180d94aca6849f5
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-04-09 17:53:58 -0600

    main section compeltada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ const HomePage = () => {
         </div>
       </header>
       <section className='grid grid-cols-12 place-items-center w-full h-full'>
-        <main className='col-span-8 border-r border-gray-300 w-full h-full'>
-          <div className='flex flex-col space-y-4 w-full py-10 px-24'>
+        <main className='col-span-8 border-r border-gray-300 w-full h-full px-24'>
+          <div className='flex flex-col space-y-4 w-full py-10'>
             <div className='flex space-x-4 items-center w-full'>
               <label htmlFor='search' className='relative w-full border-gray-800 border rounded-3xl'>
                 <div className='absolute left-2 h-full flex items-center'>
@@ -69,6 +69,61 @@ const HomePage = () => {
                 </button>
               </div>
             </div>
+          </div>
+          <div className='w-full flex flex-col justify-center space-y-8'>
+            {
+              Array.from({ length: 5 }).map((_, i) => (
+                <div key={i} className='flex group flex-col space-y-4 pb-8 border-b border-gray-300 last:border-none'>
+                  <div>
+                    <div className='flex w-full items-center space-x-2'>
+                      <div className='w-10 h-10 bg-gray-400 rounded-full'></div>
+                      <div>
+                        <p className='font-semibold'>Angel Hernández &#x2022; 23 April. 2022 </p>
+                        <p className='text-sm'>Founder, teacher & developer</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='grid grid-cols-12 w-full gap-4'>
+                    <div className='col-span-8 flex-col space-y-4'>
+                      <p className='text-2xl font-bold text-gray-800 group-hover:underline decoration-indigo-600'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Ab, blanditiis.
+                      </p>
+                      <p className='text-sm text-gray-500 break-words'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Tenetur quibusdam quos numquam, omnis deleniti perferendis
+                        aperiam doloribus, quo ipsa dolorum at unde alias? Obcaecati
+                        rem commodi distinctio, expedita dignissimos maxime hic
+                        similique natus odio, ipsum atque exercitationem? Quos
+                        perspiciatis dignissimos optio reprehenderit perferendis.
+                        Harum, quod expedita, reiciendis voluptates, numquam nam
+                        quae distinctio dolorem mollitia corporis doloremque dicta
+                        consequatur accusantium pariatur.
+                      </p>
+                    </div>
+                    <div className='col-span-4'>
+                      <div className='bg-gray-300 w-full h-full rounded-xl transition hover:scale-105 transform duration-300 hover:shadow-xl'></div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className='flex items-center w-full space-x-4 justify-start py-4'>
+                      <div>
+                        My topics:
+                      </div>
+                      <div className='flex space-x-2 items-center'>
+                        {
+                          Array.from({ length: 4 }).map((_, i) => (
+                            <div key={i} className='rounded-3xl bg-gray-200/50 px-5 py-2'>
+                              tag {i}
+                            </div>
+                          ))
+                        }
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
           </div>
         </main>
         <aside className='col-span-4 w-full h-full'>
