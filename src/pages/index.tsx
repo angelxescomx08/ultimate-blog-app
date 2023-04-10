1836ecd01e9bc96dee5c18bf66eff7538e263a27
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-04-09 18:53:59 -0600

    aside section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { HiChevronDown } from 'react-icons/hi'
 
 const HomePage = () => {
   return (
-    <div className='flex flex-col w-full h-screen'>
+    <div className='flex flex-col w-full h-full'>
       <header className='h-20 w-full flex flex-row justify-around items-center bg-white border-b-[1px] border-gray-300'>
         <div>
           <IoReorderThreeOutline className='text-2xl text-gray-600' />
@@ -29,7 +29,7 @@ const HomePage = () => {
           </div>
         </div>
       </header>
-      <section className='grid grid-cols-12 place-items-center w-full h-full'>
+      <section className='grid grid-cols-12'>
         <main className='col-span-8 border-r border-gray-300 w-full h-full px-24'>
           <div className='flex flex-col space-y-4 w-full py-10'>
             <div className='flex space-x-4 items-center w-full'>
@@ -126,8 +126,50 @@ const HomePage = () => {
             }
           </div>
         </main>
-        <aside className='col-span-4 w-full h-full'>
-          aside
+        <aside className='col-span-4 w-full p-6 flex flex-col space-y-4'>
+          <div>
+            <h3 className='my-6 font-semibold text-lg'>People you might be interested</h3>
+            <div className='flex flex-col space-4-4'>
+              {
+                Array.from({ length: 4 }).map((_, i) => (
+                  <div key={i} className='flex flex-row space-x-5 items-center'>
+                    <div className='bg-gray-300 w-10 h-10 rounded-full flex-none'></div>
+                    <div>
+                      <div className='text-gray-900 font-bold text-sm'>John Doe</div>
+                      <div className='text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, unde.</div>
+                    </div>
+                    <div>
+                      <button className='flex transition hover:border-gray-900 hover:text-gray-900 rounded items-center space-x-2 px-4 py-2.5 border border-gray-200'>
+                        Follow
+                      </button>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+          <div>
+            <h3 className='my-6 font-semibold text-lg'>Your reading list</h3>
+            <div className='flex flex-col space-y-8'>
+              {
+                Array.from({ length: 4 }).map((_, i) => (
+                  <div key={i} className='flex space-x-6 items-center group'>
+                    <div className='h-full w-2/5 bg-gray-300 rounded-xl aspect-square'></div>
+                    <div className='w-3/5 flex flex-col space-y-2'>
+                      <div className='text-lg font-semibold group-hover:underline decoration-indigo-600'>Lorem ipsum dolor sit amet consectetur.</div>
+                      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nesciunt tempora nisi a nulla ipsam amet deserunt recusandae.</div>
+                      <div className='flex space-x-1 items-center w-full'>
+                        <div className='w-8 h-8 bg-gray-300 rounded-full'></div>
+                        <div>Ángel Hernández &#x2022; </div>
+                        <div>Dec 22, 2022</div>
+                      </div>
+                    </div>
+
+                  </div>
+                ))
+              }
+            </div>
+          </div>
         </aside>
       </section>
     </div>
