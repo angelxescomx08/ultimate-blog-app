d85a4635ff65c8486d87480bbc9197e178935171
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-04-09 22:16:26 -0600

    hacer inicio de sesión

---
+++ src/layouts/MainLayout/index.tsx
@@ -0,0 +1,77 @@
+import { type ReactNode, type FC } from 'react'
+
+import { IoReorderThreeOutline } from 'react-icons/io5'
+import { BsBell } from 'react-icons/bs'
+import { FiEdit } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { HiLogout } from 'react-icons/hi';
+
+interface Props {
+    children: ReactNode;
+}
+
+const MainLayout: FC<Props> = ({ children }) => {
+    const { data: sessionData, status } = useSession()
+
+    const logIn = async () => {
+        await signIn()
+    }
+
+    const logOut = async () => {
+        await signOut()
+    }
+
+    return (
+        <div className='flex flex-col w-full h-full'>
+            <header className='h-20 w-full flex flex-row justify-around items-center bg-white border-b-[1px] border-gray-300'>
+                <div>
+                    <IoReorderThreeOutline className='text-2xl text-gray-600' />
+                </div>
+                <div className='font-thin text-xl'>Ultimate Blog App</div>
+                {
+                    status === 'authenticated'
+                        ? (<div className='flex items-center space-x-2'>
+                            <div>
+                                <BsBell className='text-2xl text-gray-600' />
+                            </div>
+                            <div>
+                                <div className='w-5 h-5 bg-gray-600 rounded-full'></div>
+                            </div>
+                            <div>
+                                <button className='flex transition hover:border-gray-900 hover:text-gray-900 rounded items-center space-x-2 px-4 py-2.5 border border-gray-200'>
+                                    <div>Write</div>
+                                    <div>
+                                        <FiEdit />
+                                    </div>
+                                </button>
+                            </div>
+                            <div>
+                                <button
+                                    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                                    onClick={logOut}
+                                    className='flex transition hover:border-gray-900 hover:text-gray-900 rounded items-center space-x-2 px-4 py-2.5 border border-gray-200'>
+                                    <div>Logout</div>
+                                    <div>
+                                        <HiLogout />
+                                    </div>
+                                </button>
+                            </div>
+                        </div>)
+                        : <div>
+                            <button
+                                // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                                onClick={logIn}
+                                className='flex transition hover:border-gray-900 hover:text-gray-900 rounded items-center space-x-2 px-4 py-2.5 border border-gray-200'>
+
+                                Signin
+                            </button>
+                        </div>
+                }
+
+            </header>
+            {children}
+        </div>
+    )
+}
+
+export default MainLayout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,11 @@
-import { IoReorderThreeOutline } from 'react-icons/io5'
-import { BsBell } from 'react-icons/bs'
-import { FiEdit } from 'react-icons/fi'
 import { CiSearch } from 'react-icons/ci'
 import { HiChevronDown } from 'react-icons/hi'
+import { MainLayout } from '~/layouts'
 
 const HomePage = () => {
   return (
-    <div className='flex flex-col w-full h-full'>
-      <header className='h-20 w-full flex flex-row justify-around items-center bg-white border-b-[1px] border-gray-300'>
-        <div>
-          <IoReorderThreeOutline className='text-2xl text-gray-600' />
-        </div>
-        <div className='font-thin text-xl'>Ultimate Blog App</div>
-        <div className='flex items-center space-x-2'>
-          <div>
-            <BsBell className='text-2xl text-gray-600' />
-          </div>
-          <div>
-            <div className='w-5 h-5 bg-gray-600 rounded-full'></div>
-          </div>
-          <div>
-            <button className='flex transition hover:border-gray-900 hover:text-gray-900 rounded items-center space-x-2 px-4 py-2.5 border border-gray-200'>
-              <div>Write</div>
-              <div>
-                <FiEdit />
-              </div>
-            </button>
-          </div>
-        </div>
-      </header>
+    <MainLayout>
+
       <section className='grid grid-cols-12'>
         <main className='col-span-8 border-r border-gray-300 w-full h-full px-24'>
           <div className='flex flex-col space-y-4 w-full py-10'>
@@ -172,7 +149,7 @@ const HomePage = () => {
           </div>
         </aside>
       </section>
-    </div>
+    </MainLayout>
   )
 }
 
