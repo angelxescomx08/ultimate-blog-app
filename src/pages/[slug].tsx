79a566a6838a6d7e5d2247ef30ebe10f34ac9eed
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-04-22 19:56:12 -0600

    like y dislike

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,14 +1,36 @@
 import { useRouter } from 'next/router'
 import { AiOutlineLoading3Quarters } from 'react-icons/ai'
+import { FcLike, FcLikePlaceholder } from 'react-icons/fc'
+import { BsChat } from 'react-icons/bs'
 import { MainLayout } from '~/layouts'
 import { api } from '~/utils/api'
+import { useCallback } from 'react'
 
 const PostPage = () => {
     const router = useRouter()
 
+    const postRoute = api.useContext().post;
+
     const post = api.post.getPost.useQuery({ slug: router.query.slug as string }, {
         //solo hacer la consulta cuando slug esta definido
         enabled: !!router.query.slug
+    })
+
+    const invalidateCurrentPostPage = useCallback(() => {
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        postRoute.getPost.invalidate({ slug: router.query.slug as string })
+    }, [])
+
+    const likePost = api.post.likePost.useMutation({
+        onSuccess: () => {
+            invalidateCurrentPostPage()
+        }
+    })
+
+    const dislikePost = api.post.dislikePost.useMutation({
+        onSuccess: () => {
+            invalidateCurrentPostPage()
+        }
     })
 
     return (
@@ -23,6 +45,32 @@ const PostPage = () => {
                     </div>
                 </div>
             }
+
+            {
+                post.isSuccess && <div className='fixed bottom-10 w-full flex justify-center'>
+                    <div className='group flex items-center space-x-4 rounded-full transition duration-300 px-6 py-3 bg-white border-[0.5px] border-gray-400 hover:border-gray-900'>
+                        <div className='border-r pr-4 group-hover:border-gray-900 transition duration-300 cursor-pointer'>
+                            {post.data?.likes && post.data?.likes.length > 0
+                                ? <FcLike
+                                    onClick={() => post.data?.id && dislikePost.mutate({
+                                        postId: post.data?.id,
+                                    })}
+                                    className='text-xl'
+                                />
+                                : <FcLikePlaceholder
+                                    onClick={() => post.data?.id && likePost.mutate({
+                                        postId: post.data?.id,
+                                    })}
+                                    className='text-xl'
+                                />}
+                        </div>
+                        <div>
+                            <BsChat className='text-base' />
+                        </div>
+                    </div>
+                </div>
+            }
+
             <div className='flex flex-col h-full w-full justify-center items-center p-10'>
                 <div className='max-w-screen-lg w-full flex flex-col space-y-6'>
                     <div className='h-[60vh] relative w-full rounded-xl bg-gray-300 shadow-lg'>
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -47,12 +47,52 @@ export const postRouter = createTRPCRouter({
         .input(z.object({
             slug: z.string()
         }))
-        .query(async ({ ctx: { prisma }, input: { slug } }) => {
+        .query(async ({ ctx: { prisma, session }, input: { slug } }) => {
             const post = await prisma.post.findUnique({
                 where: {
                     slug
+                },
+                select: {
+                    id: true,
+                    description: true,
+                    title: true,
+                    text: true,
+                    likes: session?.user?.id ? {
+                        where: {
+                            userId: session?.user.id
+                        }
+                    } : false
                 }
             })
             return post
-        })
+        }),
+
+    likePost: protectedProcedure
+        .input(z.object({
+            postId: z.string()
+        }))
+        .mutation(async ({ ctx: { prisma, session }, input: { postId } }) => {
+            await prisma.like.create({
+                data: {
+                    postId,
+                    userId: session.user.id
+                }
+            })
+        }),
+
+    dislikePost: protectedProcedure
+        .input(z.object({
+            postId: z.string()
+        }))
+        .mutation(async ({ ctx: { prisma, session }, input: { postId } }) => {
+            await prisma.like.delete({
+                where: {
+                    userId_postId: {
+                        userId: session.user.id,
+                        postId
+                    }
+                }
+            })
+        }),
+
 })
