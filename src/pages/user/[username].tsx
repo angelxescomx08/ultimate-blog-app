eb63be624e5578b9149bb0b882f48ef3acdd2314
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-05-01 18:45:31 -0600

    traer los post del usuario

--- src/pages/user/[username].tsx
+++ src/pages/user/[username].tsx
@@ -4,6 +4,10 @@ import React from 'react'
 import { MainLayout } from '~/layouts'
 import { api } from '~/utils/api'
 import { BiEdit } from 'react-icons/bi'
+import { toast } from 'react-hot-toast'
+import { SlShareAlt } from 'react-icons/sl'
+import Post from '~/components/Post'
+
 
 const UserProfilePage = () => {
 
@@ -12,37 +16,72 @@ const UserProfilePage = () => {
         username: router.query.username as string
     }, { enabled: !!router.query.username })
 
+    const userPosts = api.auth.getUserPost.useQuery({
+        username: router.query.username as string
+    }, { enabled: !!router.query.username })
+
     return (
         <MainLayout>
             <div className='w-full h-full flex items-center justify-center'>
                 <div className='my-10 flex flex-col justify-center items-center xl:max-w-screen-lg lg:max-w-screen-md w-full h-full'>
-                    <div className='relative h-44 bg-gradient-to-br w-full rounded-3xl from-rose-100 to-teal-100'>
-                        <div className='absolute -bottom-10 left-12'>
-                            <div className='group w-32 h-32 rounded-full relative border-white border-2 bg-gray-100 cursor-pointer'>
-                                <label htmlFor='avatarFile' className='cursor-pointer absolute flex items-center justify-center w-full h-full group-hover:bg-black/20 z-10 rounded-full transition'>
-                                    <BiEdit className='text-3xl hidden group-hover:block text-white' />
-                                    <input
-                                        type='file'
-                                        name='avatarFile'
-                                        id='avatarFile'
-                                        className='sr-only'
-                                        accept='image/*'
-                                    />
-                                </label>
-                                {
-                                    userProfile.data?.image &&
-                                    <Image
-                                        className='rounded-full'
-                                        src={userProfile.data?.image}
-                                        alt={userProfile.data?.name ?? ''}
-                                        fill
-                                    />
-                                }
+                    <div className='bg-white rounded-3xl flex flex-col w-full shadow-md'>
+                        <div className='relative h-44 bg-gradient-to-br w-full rounded-t-3xl from-rose-100 to-teal-100'>
+                            <div className='absolute -bottom-10 left-12'>
+                                <div className='group w-32 h-32 rounded-full relative border-white border-2 bg-gray-100 cursor-pointer'>
+                                    <label htmlFor='avatarFile' className='cursor-pointer absolute flex items-center justify-center w-full h-full group-hover:bg-black/20 z-10 rounded-full transition'>
+                                        <BiEdit className='text-3xl hidden group-hover:block text-white' />
+                                        <input
+                                            type='file'
+                                            name='avatarFile'
+                                            id='avatarFile'
+                                            className='sr-only'
+                                            accept='image/*'
+                                        />
+                                    </label>
+                                    {
+                                        userProfile.data?.image &&
+                                        <Image
+                                            className='rounded-full'
+                                            src={userProfile.data?.image}
+                                            alt={userProfile.data?.name ?? ''}
+                                            fill
+                                        />
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mt-10 ml-12 flex flex-col space-y-0.5 rounded-b-3xl py-5'>
+                            <div className='text-2xl font-semibold text-gray-800'>
+                                {userProfile.data?.name}
+                            </div>
+                            <div className='text-gray-600'>
+                                @{userProfile.data?.username}
+                            </div>
+                            <div className='text-gray-600'>
+                                {userProfile.data?._count.posts ?? 0} Posts
+                            </div>
+                            <div>
+                                <button
+                                    onClick={() => {
+                                        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+                                        navigator.clipboard.writeText(window.location.href)
+                                        toast.success('URL copied')
+                                    }}
+                                    className='mt-2 flex transition hover:border-gray-900 hover:text-gray-900 rounded items-center space-x-3 px-4 py-2 border border-gray-200 active:scale-95 transform '>
+                                    <div>Write</div>
+                                    <div>
+                                        <SlShareAlt />
+                                    </div>
+                                </button>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        Post
+                    <div className='my-10 w-full'>
+                        {
+                            userPosts.isSuccess && userPosts.data?.posts.map((post) => (
+                                <Post {...post} key={post.id} />
+                            ))
+                        }
                     </div>
                 </div>
             </div>
--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -15,7 +15,46 @@ export const authRouter = createTRPCRouter({
                     name: true,
                     image: true,
                     id: true,
-                    username: true
+                    username: true,
+                    _count: {
+                        select: {
+                            posts: true
+                        }
+                    }
+                }
+            })
+        }),
+    getUserPost: publicProcedure
+        .input(z.object({
+            username: z.string()
+        }))
+        .query(async ({ ctx: { prisma, session }, input: { username } }) => {
+            return await prisma.user.findUnique({
+                where: {
+                    username
+                },
+                select: {
+                    posts: {
+                        select: {
+                            id: true,
+                            slug: true,
+                            title: true,
+                            description: true,
+                            createdAt: true,
+                            author: {
+                                select: {
+                                    name: true,
+                                    image: true,
+                                    username: true
+                                }
+                            },
+                            bookmarks: session?.user.id ? {
+                                where: {
+                                    userId: session?.user.id
+                                }
+                            } : false
+                        }
+                    }
                 }
             })
         })
